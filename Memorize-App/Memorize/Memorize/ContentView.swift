659696b685ec4000bb9e362ba152af5c41d53f5a
Luis Ramirez <lramirezsuarez@gmail.com>
2022-07-07 08:05:43 -0500

    Memorize project.

---
+++ Memorize-App/Memorize/Memorize/ContentView.swift
@@ -0,0 +1,25 @@
+//
+//  ContentView.swift
+//  Memorize
+//
+//  Created by Luis Alejandro Ramirez Suarez on 6/07/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hola lo mejor!")
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Memorize-App/Memorize/Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Luis Alejandro Ramirez Suarez on 6/07/22.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
